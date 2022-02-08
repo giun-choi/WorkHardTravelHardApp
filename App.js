62cfc921c5b4b52a3e3efd3f31aa2fe8f1e5f06d
giun-choi <liamg8341@gmail.com>
2022-02-09 00:33:44 +0900

    Touchable

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from 'react-native';
+import { theme } from './colors';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableWithoutFeedback>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20
+  },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 100
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: '600',
+    color: 'white'
+  }
+});
